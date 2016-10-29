07bfe3db208c458c51fe052bde647c67d96ca118
Jonathan Doron <yonidn7@gmail.com>
2016-10-29 11:26:13 +0300

    final fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var uniqueRandomArray = require('unique-random-array');
 var gameOfThronesNames  = require('./gameOfThrones-names.json');
 
-// get a random name
 module.exports = {
     all: gameOfThronesNames,
     random: uniqueRandomArray(gameOfThronesNames)
